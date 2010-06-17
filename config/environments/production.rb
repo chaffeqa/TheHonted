47c5662ae309df97532b1089a7c2a492d4fb5145
chaffeqa <chaffeqa@gmail.com>
2010-06-17 12:15:34 -0400

    fixed the static assets for production test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,8 @@ Thehonted::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  # Full error reports are disabled and caching is turned on TODO set local = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
@@ -27,9 +27,9 @@ Thehonted::Application.configure do
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
-  # Disable Rails's static asset server
+  # Disable Rails's static asset server TODO change to false
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
